3a2bd258ce9e7f7edb4e45880279eee91b3f4870
Monica Sue <monicasue567@gmail.com>
2023-05-12 11:38:01 -0400

    add itinerary card component outline

---
+++ src/components/ItineraryCard/ItineraryCard.jsx
@@ -0,0 +1,13 @@
+// npm module
+import { Link } from 'react-router-dom';
+
+// css
+import styles from './ItineraryCard.module.css'
+
+const ItineraryCard = () => {
+  return (
+
+  )
+}
+
+export default ItineraryCard;
