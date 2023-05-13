9ba38a643365f588a184593cfac5bc2dec05dfc1
CurrentlyBobe <walshbobby5@gmail.com>
2023-05-13 18:12:06 -0500

    add edit accommodation view

--- src/pages/EditAccommodations/EditAccommodations.jsx
+++ src/pages/EditAccommodations/EditAccommodations.jsx
@@ -6,7 +6,92 @@ import styles from './EditAccommodation.module.css'
 import * as itineraryService from '../../services/itineraryService'
 
 const EditAccommodation = () => {
-    return (  );
+    const navigate = useNavigate()
+    const { state } = useLocation()
+    const { itineraryId, accommodationId } = useParams()
+    const [formData, setFormData] = useState(state)
+
+    const handleChange = ({ target }) => {
+        setFormData({ ...formData, [target.name]: target.value })
+    }
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault()
+        await itineraryService.updateAccommodation(itineraryId, accommodationId, formData)
+        navigate(`/itineraries/${itineraryId}`)
+    }
+    
+    return ( 
+        <form className={styles.container} onSubmit={handleSubmit}>
+      <h2>Edit Accommodation</h2>
+      <label htmlFor="type-input">Type</label>
+        <select 
+          name="type" 
+          id="type-input"
+          value={formData.type}
+          onChange={handleChange}
+        >
+          <option value="Hotel">Hotel</option>
+          <option value="Cruise">Cruise</option>
+          <option value="Airbnb">Airbnb</option>
+          <option value="Other">Other</option>
+        </select>
+      <label htmlFor="name">Name</label>
+      <input 
+        type="text" 
+        name="name"
+        id="name-input"
+        value={formData.name}
+        autoComplete='off'
+        onChange={handleChange}
+      />
+      <label htmlFor="checkInDate">CheckIn Date</label>
+      <input 
+        required
+        type="date" 
+        name="checkInDate"
+        id="checkInDate-input"
+        value={formData.checkInDate}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkOutDate">CheckOut Date</label>
+      <input 
+        type="date" 
+        name="checkOutDate"
+        id="checkOutDate-input"
+        value={formData.checkOutDate}
+        onChange={handleChange}
+      />
+      <label htmlFor="address">Address</label>
+      <input 
+        type="text" 
+        name="address"
+        id="address-input"
+        value={formData.address}
+        autoComplete='off'
+        onChange={handleChange}
+      />
+      <label htmlFor="website">Website</label>
+      <input 
+        type="text" 
+        name="website"
+        id="website-input"
+        value={formData.website}
+        autoComplete='off'
+        onChange={handleChange}
+      />
+      <label htmlFor="cost">Cost</label>
+      <input 
+        type="text" 
+        name="cost"
+        id="cost-input"
+        value={formData.cost}
+        autoComplete='off'
+        onChange={handleChange}
+      />
+      <button type="submit">SUBMIT</button>
+    </form> 
+     );
 }
  
 export default EditAccommodation;
