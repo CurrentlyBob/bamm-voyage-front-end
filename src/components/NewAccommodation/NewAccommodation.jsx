4646fa9ae936bbbf65c12c1424e01460f364c391
Monica Sue <monicasue567@gmail.com>
2023-05-14 18:56:54 -0400

    add toggle to accommodation form

--- src/components/NewAccommodation/NewAccommodation.jsx
+++ src/components/NewAccommodation/NewAccommodation.jsx
@@ -1,5 +1,6 @@
 // npm imports
 import { useEffect, useState } from 'react'
+import Switch from '@mui/material/Switch'
 
 
 // css
@@ -10,6 +11,12 @@ import * as itineraryService from '../../services/itineraryService'
 
 
 const NewAccommodation = (props) => {
+  const [showForm, setShowForm] = useState(false)
+  
+  const toggle = async () => {
+    showForm ? setShowForm(false) : setShowForm(true)
+  }
+
   const [formData, setFormData]= useState({
     type: 'Hotel',
     name: '',
@@ -39,76 +46,86 @@ const NewAccommodation = (props) => {
   }
 
   return ( 
-    <form className={styles.container} onSubmit={handleSubmit}>
-      <h2>Accommodation</h2>
-      <label htmlFor="type-input">Type</label>
-        <select 
-          name="type" 
-          id="type-input"
-          value={formData.type}
-          onChange={handleChange}
-        >
-          <option value="Hotel">Hotel</option>
-          <option value="Cruise">Cruise</option>
-          <option value="Airbnb">Airbnb</option>
-          <option value="Other">Other</option>
-        </select>
-      <label htmlFor="name">Name</label>
-      <input 
-        type="text" 
-        name="name"
-        id="name-input"
-        value={formData.name}
-        autoComplete='off'
-        onChange={handleChange}
+    <>
+      <Switch 
+        onChange={toggle}
       />
-      <label htmlFor="checkInDate">CheckIn Date</label>
-      <input 
-        required
-        type="date" 
-        name="checkInDate"
-        id="checkInDate-input"
-        value={formData.checkInDate}
-        onChange={handleChange}
-      />
-      <label htmlFor="checkOutDate">CheckOut Date</label>
-      <input 
-        type="date" 
-        name="checkOutDate"
-        id="checkOutDate-input"
-        value={formData.checkOutDate}
-        onChange={handleChange}
-      />
-      <label htmlFor="address">Address</label>
-      <input 
-        type="text" 
-        name="address"
-        id="address-input"
-        value={formData.address}
-        autoComplete='off'
-        onChange={handleChange}
-      />
-      <label htmlFor="website">Website</label>
-      <input 
-        type="text" 
-        name="website"
-        id="website-input"
-        value={formData.website}
-        autoComplete='off'
-        onChange={handleChange}
-      />
-      <label htmlFor="cost">Cost</label>
-      <input 
-        type="text" 
-        name="cost"
-        id="cost-input"
-        value={formData.cost}
-        autoComplete='off'
-        onChange={handleChange}
-      />
-      <button type="submit">SUBMIT</button>
-    </form>
+      { showForm 
+        ? 
+        <form className={styles.container} onSubmit={handleSubmit}>
+          <h2>Accommodation</h2>
+          <label htmlFor="type-input">Type</label>
+            <select 
+              name="type" 
+              id="type-input"
+              value={formData.type}
+              onChange={handleChange}
+            >
+              <option value="Hotel">Hotel</option>
+              <option value="Cruise">Cruise</option>
+              <option value="Airbnb">Airbnb</option>
+              <option value="Other">Other</option>
+            </select>
+          <label htmlFor="name">Name</label>
+          <input 
+            type="text" 
+            name="name"
+            id="name-input"
+            value={formData.name}
+            autoComplete='off'
+            onChange={handleChange}
+          />
+          <label htmlFor="checkInDate">CheckIn Date</label>
+          <input 
+            required
+            type="date" 
+            name="checkInDate"
+            id="checkInDate-input"
+            value={formData.checkInDate}
+            onChange={handleChange}
+          />
+          <label htmlFor="checkOutDate">CheckOut Date</label>
+          <input 
+            type="date" 
+            name="checkOutDate"
+            id="checkOutDate-input"
+            value={formData.checkOutDate}
+            onChange={handleChange}
+          />
+          <label htmlFor="address">Address</label>
+          <input 
+            type="text" 
+            name="address"
+            id="address-input"
+            value={formData.address}
+            autoComplete='off'
+            onChange={handleChange}
+          />
+          <label htmlFor="website">Website</label>
+          <input 
+            type="text" 
+            name="website"
+            id="website-input"
+            value={formData.website}
+            autoComplete='off'
+            onChange={handleChange}
+          />
+          <label htmlFor="cost">Cost</label>
+          <input 
+            type="text" 
+            name="cost"
+            id="cost-input"
+            value={formData.cost}
+            autoComplete='off'
+            onChange={handleChange}
+          />
+          <button type="submit">SUBMIT</button>
+        </form>
+      :
+        <h4>Add Accommodation</h4>
+      }
+    </>
   )
 }
- 
+
 export default NewAccommodation;
