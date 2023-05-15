58e8addb5521351f05e99000951d9948fbe90456
CurrentlyBobe <walshbobby5@gmail.com>
2023-05-15 12:44:48 -0500

    remove date utility functionality from editFlight

--- src/DateUtility/DateUtility.jsx
+++
@@ -1,6 +0,0 @@
-export function subtractOneDay(dateValue) {
-    const dateProvided = new Date(dateValue)
-    dateProvided.setDate(dateProvided.getDate() - 1)
-    const adjustedDate = dateProvided.toISOString().split('T')[0]
-    return adjustedDate
-}
