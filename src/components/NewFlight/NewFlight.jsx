34650d9bf8e97de23115ccec1bcdd346c23dc1d8
Monica Sue <monicasue567@gmail.com>
2023-05-16 14:55:12 -0400

    tweak new flight form with fieldset and legend elements

--- src/components/NewFlight/NewFlight.jsx
+++ src/components/NewFlight/NewFlight.jsx
@@ -49,96 +49,117 @@ const NewFlight = (props) => {
   return (
     <>
     <div>
-    <FormGroup className={styles.toggleFormContainer}>
-      <FormControlLabel 
-      control={ <Switch 
-        onChange={toggle}
-        className={styles.toggle}
-      />} 
-      label={`${showForm? '' : 'Add Flight'}`}
-      labelPlacement="top"
-      className={styles.toggleContainer} />
-    </FormGroup>
-    { showForm 
-    ? 
-      <form className={`${styles.container} ${!showForm && styles.hidden}`} onSubmit={handleSubmit}>
-        <h2>Flight</h2>
-        <label htmlFor="airline-input">Airline</label>
-            <input 
-              required
-              type="text" 
-              name="airline"
-              id="airline-input"
-              autoComplete='off'
-              value={formData.airline}
-              placeholder="Airline Name"
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <label htmlFor="departureAirport">Departure Airport</label>
-            <input 
-              required
-              type="text" 
-              name="departureAirport"
-              id="departureAirport-input"
-              autoComplete='off'
-              value={formData.departureAirport}
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <label htmlFor="departureDate">Departure Date</label>
-            <input 
-              type="datetime-local" 
-              name="departureDate"
-              id="departureDate-input"
-              value={formData.departureDate}
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <label htmlFor="arrivalAirport">Arrival Airport</label>
-            <input 
-              type="text" 
-              name="arrivalAirport"
-              id="arrivalAirport-input"
-              autoComplete='off'
-              value={formData.arrivalAirport}
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <label htmlFor="arrivalDate">Arrival Date</label>
-            <input 
-              type="datetime-local" 
-              name="arrivalDate"
-              id="arrivalDate-input"
-              value={formData.arrivalDate}
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <label htmlFor="recordLocator">Record Locator</label>
-            <input 
-              type="text" 
-              name="recordLocator"
-              id="recordLocator-input"
-              autoComplete='off'
-              value={formData.recordLocator}
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <label htmlFor="cost">Cost</label>
-            <input 
-              type="text" 
-              name="cost"
-              id="cost-input"
-              autoComplete='off'
-              value={formData.cost}
-              onChange={handleChange}
-              className={styles.input}
-            />
-            <button type="submit">SUBMIT</button>
-      </form>
-    :
-      <></>
-    }
+      <FormGroup className={styles.toggleFormContainer}>
+        <FormControlLabel 
+        control={ <Switch 
+          onChange={toggle}
+          className={styles.toggle}
+        />} 
+        label={`${showForm? '' : 'Add Flight'}`}
+        labelPlacement="top"
+        className={styles.toggleContainer} />
+      </FormGroup>
+      { showForm 
+      ? 
+        <form className={`${styles.container} ${!showForm && styles.hidden}`} onSubmit={handleSubmit}>
+          <h2>Flight</h2>
+          {/* <label htmlFor="airline-input">Airline</label> */}
+              <fieldset>
+                <legend>Airline</legend>
+                <input 
+                  required
+                  type="text" 
+                  name="airline"
+                  // id="airline-input"
+                  autoComplete='off'
+                  value={formData.airline}
+                  // placeholder="Airline Name"
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+              </fieldset>
+              {/* <label htmlFor="departureAirport">Departure Airport</label> */}
+              <fieldset>
+                <legend>Departure Airport</legend>
+                <input 
+                  required
+                  type="text" 
+                  name="departureAirport"
+                  // id="departureAirport-input"
+                  autoComplete='off'
+                  value={formData.departureAirport}
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+              </fieldset>
+              {/* <label htmlFor="departureDate">Departure Date</label> */}
+              <fieldset>
+                <legend>Departure Date</legend>
+                <input 
+                  type="datetime-local" 
+                  name="departureDate"
+                  // id="departureDate-input"
+                  value={formData.departureDate}
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+              </fieldset>
+              {/* <label htmlFor="arrivalAirport">Arrival Airport</label> */}
+              <fieldset>
+                <legend>Arrival Airport</legend>
+                <input 
+                  type="text" 
+                  name="arrivalAirport"
+                  // id="arrivalAirport-input"
+                  autoComplete='off'
+                  value={formData.arrivalAirport}
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+              </fieldset>
+              {/* <label htmlFor="arrivalDate">Arrival Date</label> */}
+              <fieldset>
+                <legend>Arrival Date</legend>
+                <input 
+                  type="datetime-local" 
+                  name="arrivalDate"
+                  // id="arrivalDate-input"
+                  value={formData.arrivalDate}
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+              </fieldset>
+              {/* <label htmlFor="recordLocator">Record Locator</label> */}
+              <fieldset>
+                <legend>Record Locator</legend>
+                <input 
+                  type="text" 
+                  name="recordLocator"
+                  id="recordLocator-input"
+                  autoComplete='off'
+                  value={formData.recordLocator}
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+              </fieldset>
+              {/* <label htmlFor="cost">Cost</label> */}
+              <fieldset>
+                <legend>Cost</legend>
+                <input 
+                  type="text" 
+                  name="cost"
+                  id="cost-input"
+                  autoComplete='off'
+                  value={formData.cost}
+                  onChange={handleChange}
+                  className={styles.input}
+                />
+                </fieldset>
+              <button type="submit">SUBMIT</button>
+        </form>
+      :
+        <></>
+      }
     </div>
   </>
   )
