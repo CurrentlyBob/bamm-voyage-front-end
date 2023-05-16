88cdb9c513462d6aa9d4ba0eea20a8d97003dca6
Monica Sue <monicasue567@gmail.com>
2023-05-16 19:26:13 -0400

    edit edit flight form

--- src/pages/EditFlight/EditFlight.jsx
+++ src/pages/EditFlight/EditFlight.jsx
@@ -25,73 +25,104 @@ const EditFlight = () => {
   };
 
   return (
-    <form className={styles.container} onSubmit={handleSubmit}>
-      <label htmlFor="airline-input">Airline</label>
-      <input
-        required
-        type="text"
-        name="airline"
-        id="airline-input"
-        autoComplete="off"
-        value={formData.airline}
-        placeholder="Airline Name"
-        onChange={handleChange}
-      />
-      <label htmlFor="departureAirport">Departure Airport</label>
-      <input
-        required
-        type="text"
-        name="departureAirport"
-        id="departureAirport-input"
-        autoComplete="off"
-        value={formData.departureAirport}
-        onChange={handleChange}
-      />
-      <label htmlFor="departureDate">Departure Date</label>
-      <input
-        type="datetime-local"
-        name="departureDate"
-        id="departureDate-input"
-        value={moment(formData.departureDate).format('yyyy-MM-DD HH:mm')}
-        onChange={handleChange}
-      />
-      <label htmlFor="arrivalAirport">Arrival Airport</label>
-      <input
-        type="text"
-        name="arrivalAirport"
-        id="arrivalAirport-input"
-        autoComplete="off"
-        value={formData.arrivalAirport}
-        onChange={handleChange}
-      />
-      <label htmlFor="arrivalDate">Arrival Date</label>
-      <input
-        type="datetime-local"
-        name="arrivalDate"
-        id="arrivalDate-input"
-        value={moment(formData.arrivalDate).format('yyyy-MM-DD HH:mm')}
-        onChange={handleChange}
-      />
-      <label htmlFor="recordLocator">Record Locator</label>
-      <input
-        type="text"
-        name="recordLocator"
-        id="recordLocator-input"
-        autoComplete="off"
-        value={formData.recordLocator}
-        onChange={handleChange}
-      />
-      <label htmlFor="cost">Cost</label>
-      <input
-        type="text"
-        name="cost"
-        id="cost-input"
-        autoComplete="off"
-        value={formData.cost}
-        onChange={handleChange}
-      />
-      <button type="submit">SUBMIT</button>
-    </form>
+    <>
+      <h1 className={styles.title}>Edit Flight</h1>
+      <form className={styles.container} onSubmit={handleSubmit}>
+        {/* <label htmlFor="airline-input">Airline</label> */}
+        <fieldset>
+          <legend>Airline</legend>
+          <input 
+            required
+            type="text" 
+            name="airline"
+            // id="airline-input"
+            autoComplete='off'
+            value={formData.airline}
+            // placeholder="Airline Name"
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        {/* <label htmlFor="departureAirport">Departure Airport</label> */}
+        <fieldset>
+          <legend>Departure Airport</legend>
+          <input 
+            required
+            type="text" 
+            name="departureAirport"
+            // id="departureAirport-input"
+            autoComplete='off'
+            value={formData.departureAirport}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        {/* <label htmlFor="departureDate">Departure Date</label> */}
+        <fieldset>
+          <legend>Departure Date</legend>
+          <input 
+            type="datetime-local" 
+            name="departureDate"
+            // id="departureDate-input"
+            value={formData.departureDate}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        {/* <label htmlFor="arrivalAirport">Arrival Airport</label> */}
+        <fieldset>
+          <legend>Arrival Airport</legend>
+          <input 
+            type="text" 
+            name="arrivalAirport"
+            // id="arrivalAirport-input"
+            autoComplete='off'
+            value={formData.arrivalAirport}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        {/* <label htmlFor="arrivalDate">Arrival Date</label> */}
+        <fieldset>
+          <legend>Arrival Date</legend>
+          <input 
+            type="datetime-local" 
+            name="arrivalDate"
+            // id="arrivalDate-input"
+            value={formData.arrivalDate}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        {/* <label htmlFor="recordLocator">Record Locator</label> */}
+        <fieldset>
+          <legend>Record Locator</legend>
+          <input 
+            type="text" 
+            name="recordLocator"
+            id="recordLocator-input"
+            autoComplete='off'
+            value={formData.recordLocator}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        {/* <label htmlFor="cost">Cost</label> */}
+        <fieldset>
+          <legend>Cost</legend>
+          <input 
+            type="text" 
+            name="cost"
+            id="cost-input"
+            autoComplete='off'
+            value={formData.cost}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </fieldset>
+        <button type="submit">SUBMIT</button>
+      </form>
+    </>
   );
 };
 
