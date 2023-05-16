7e5c3bca2cdc1cf8d76e0d51d431e19e0b635a7c
Monica Sue <monicasue567@gmail.com>
2023-05-16 11:12:36 -0400

    add background image to logged out landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,33 +1,40 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 // css
-import styles from './Landing.module.css'
+import styles from "./Landing.module.css";
 
 const Landing = ({ user }) => {
   return (
     <>
-      {user 
-        ? 
+      {user ? (
+        <main className={styles.loggedInBackground}>
           <div className={styles.container}>
-            <h1>hello, {user ? user.name : 'friend'}</h1>
+            <h1>hello, {user ? user.name : "friend"}</h1>
           </div>
-        :
+        </main>
+      ) : (
+        <main className={styles.loggedOutBackground} >
           <div className={styles.loggedOutContainer}>
             <h1>BAMM Voyage</h1>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae voluptas magnam deleniti eligendi voluptates maxime, nobis ea, quis eveniet totam laboriosam, ullam cum quia est soluta harum corporis libero!</h3>
+            <h3 className={styles.description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur recusandae voluptas magnam deleniti eligendi voluptates
+              maxime, nobis ea, quis eveniet totam laboriosam, ullam cum quia
+              est soluta harum corporis libero!
+            </h3>
             <div className={styles.loggedOutButtonContainer}>
-              <button>
+              <button className={styles.landingButton}>
                 <NavLink to="/auth/login">Log In</NavLink>
               </button>
-              <button>
-              <NavLink to="/auth/signup">Sign Up</NavLink>
+              <button className={styles.landingButton}>
+                <NavLink to="/auth/signup">Sign Up</NavLink>
               </button>
             </div>
-
           </div>
-      }
+        </main>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
