f887386c41d0094b1e84e3a148ab60a66a1263f3
Monica Sue <monicasue567@gmail.com>
2023-05-17 13:23:57 -0400

    edit forms

--- src/pages/EditItinerary/EditItinerary.jsx
+++ src/pages/EditItinerary/EditItinerary.jsx
@@ -22,8 +22,8 @@ const EditItinerary = (props) => {
 
   return (
     <main> 
-      <h1 className={styles.title}>Edit Your Itinerary</h1> 
       <form onSubmit={handleSubmit} className={styles.container}>
+        <h1 className={styles.title}>Edit Itinerary</h1> 
         {/* <label htmlFor="title-input">Itinerary Name</label> */}
         <fieldset>
           <legend>Itinerary Name</legend>
