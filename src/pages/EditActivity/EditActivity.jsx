45ae27cfa87e873c1e1efac433e77c1837af0882
Monica Sue <monicasue567@gmail.com>
2023-05-17 19:24:49 -0400

    change editActivity background image

--- src/pages/EditActivity/EditActivity.jsx
+++ src/pages/EditActivity/EditActivity.jsx
@@ -21,93 +21,95 @@ const EditActivity = () => {
   };
 
   return (
-    <>
-      <form className={`${styles.container}`} onSubmit={handleSubmit}>
-        <h1 className={styles.title}>Edit Activity</h1>
-        {/* <label htmlFor="type-input">Category</label> */}
-        <fieldset>
-          <legend>Type</legend>
-          <select
-            name="category"
-            // id="type-input"
-            value={formData.category}
-            onChange={handleChange}
-            className={styles.select}
-          >
-            <option value="Activity">Activity</option>
-            <option value="Restaurant">Restaurant</option>
-            <option value="Landmark">Landmark</option>
-            <option value="Nightlife">Nightlife</option>
-          </select>
-        </fieldset>
-        {/* <label htmlFor="name">Activity</label> */}
-        <fieldset>
-          <legend>Name</legend>
-          <input
-            type="text"
-            name="name"
-            // id="name-input"
-            value={formData.name}
-            autoComplete="off"
-            onChange={handleChange}
-            className={styles.input}
-          />
-        </fieldset>
-        {/* <label htmlFor="activityDate">Activity Date</label> */}
-        <fieldset>
-          <legend>Activity Date</legend>
-          <input
-            required
-            type="datetime-local"
-            name="activityDate"
-            // id="activityDate-input"
-            value={moment(formData.Date).format("yyyy-MM-DD HH:mm")}
-            onChange={handleChange}
-            className={styles.input}
-          />
-        </fieldset>
-        {/* <label htmlFor="activityWebsite">Activity Website</label> */}
-        <fieldset>
-          <legend>Website</legend>
-          <input
-            type="text"
-            name="activityWebsite"
-            // id="activityWebsite-input"
-            value={formData.activityWebsite}
-            autoComplete="off"
-            onChange={handleChange}
-            className={styles.input}
-          />
-        </fieldset>
-        {/* <label htmlFor="notes">Notes</label> */}
-        <fieldset>
-          <legend>Notes</legend>
-          <input
-            type="text"
-            name="notes"
-            // id="notes-input"
-            value={formData.notes}
-            autoComplete="off"
-            onChange={handleChange}
-            className={styles.input}
-          />
-        </fieldset>
-        {/* <label htmlFor="cost">Cost</label> */}
-        <fieldset>
-          <legend>Cost</legend>
-          <input
-            type="text"
-            name="cost"
-            // id="cost-input"
-            value={formData.cost}
-            autoComplete="off"
-            onChange={handleChange}
-            className={styles.input}
-          />
-        </fieldset>
-        <button type="submit">SUBMIT</button>
-      </form>
-    </>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <form className={`${styles.form}`} onSubmit={handleSubmit}>
+          <h1 className={styles.title}>Edit Activity</h1>
+          {/* <label htmlFor="type-input">Category</label> */}
+          <fieldset>
+            <legend>Type</legend>
+            <select
+              name="category"
+              // id="type-input"
+              value={formData.category}
+              onChange={handleChange}
+              className={styles.select}
+            >
+              <option value="Activity">Activity</option>
+              <option value="Restaurant">Restaurant</option>
+              <option value="Landmark">Landmark</option>
+              <option value="Nightlife">Nightlife</option>
+            </select>
+          </fieldset>
+          {/* <label htmlFor="name">Activity</label> */}
+          <fieldset>
+            <legend>Name</legend>
+            <input
+              type="text"
+              name="name"
+              // id="name-input"
+              value={formData.name}
+              autoComplete="off"
+              onChange={handleChange}
+              className={styles.input}
+            />
+          </fieldset>
+          {/* <label htmlFor="activityDate">Activity Date</label> */}
+          <fieldset>
+            <legend>Activity Date</legend>
+            <input
+              required
+              type="datetime-local"
+              name="activityDate"
+              // id="activityDate-input"
+              value={moment(formData.Date).format("yyyy-MM-DD HH:mm")}
+              onChange={handleChange}
+              className={styles.input}
+            />
+          </fieldset>
+          {/* <label htmlFor="activityWebsite">Activity Website</label> */}
+          <fieldset>
+            <legend>Website</legend>
+            <input
+              type="text"
+              name="activityWebsite"
+              // id="activityWebsite-input"
+              value={formData.activityWebsite}
+              autoComplete="off"
+              onChange={handleChange}
+              className={styles.input}
+            />
+          </fieldset>
+          {/* <label htmlFor="notes">Notes</label> */}
+          <fieldset>
+            <legend>Notes</legend>
+            <input
+              type="text"
+              name="notes"
+              // id="notes-input"
+              value={formData.notes}
+              autoComplete="off"
+              onChange={handleChange}
+              className={styles.input}
+            />
+          </fieldset>
+          {/* <label htmlFor="cost">Cost</label> */}
+          <fieldset>
+            <legend>Cost</legend>
+            <input
+              type="text"
+              name="cost"
+              // id="cost-input"
+              value={formData.cost}
+              autoComplete="off"
+              onChange={handleChange}
+              className={styles.input}
+            />
+          </fieldset>
+          <button type="submit">SUBMIT</button>
+        </form>
+      </div>
+    </main>
   );
 };
 
