ae2757d88412dc940d11fafae7fa2fe291d97769
Andrea <andrearewards@hotmail.com>
2023-05-17 21:25:05 -0400

    change activity card format, fix the table and heading of activity card

--- src/components/ActivityCard/ActivityCard.jsx
+++ src/components/ActivityCard/ActivityCard.jsx
@@ -5,37 +5,54 @@ import styles from "./ActivityCard.module.css";
 import ActivityKebabMenu from "../KebabMenus/ActivityKebabMenu";
 
 const ActivityCard = ({ activity, itineraryId, handleDeleteActivity }) => {
-  console.log(activity)
+  console.log(activity);
   return (
     <>
-      <div className={styles.title}>
-        <h2>{activity.category}</h2>
-        <h2>{activity.name}</h2>
-        <h2>${activity.cost}</h2>
-        <ActivityKebabMenu activity={activity} itineraryId={itineraryId} handleDeleteActivity={handleDeleteActivity}/>
+      <div className={styles.activityTitleContainer}>
+        <div className={styles.title}>
+          <h2>{activity.category}:</h2>
+          <h2>{activity.name}</h2>
+        </div>
+        <h2 className={styles.activityCost}>${activity.cost}</h2>
+        <ActivityKebabMenu
+          activity={activity}
+          itineraryId={itineraryId}
+          handleDeleteActivity={handleDeleteActivity}
+        />
       </div>
-      <div className={styles.subtitle}></div>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th className={styles.th}>Activity Date</th>
-            <th className={styles.th}>Notes</th>
-            <th className={styles.th}>Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className={styles.td}>
-              {moment(activity.date).format(
-                "ddd MMMM Do, YYYY hh:mm a"
-              )}
-            </td>
-            <td className={styles.td}>{activity.notes}</td>
-            <td className={styles.td}><a className={styles.activityUrl}  href={`${activity.activityWebsite}`} target="_blank" rel="noreferrer"> {activity.name}'s website</a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles.subtitle}>
+        <div className={styles.activityDateContainer}>
+          <p>Starting:</p>
+          <p className={styles.activityDate}>
+            {moment(activity.date).format("ddd MMMM Do, YYYY hh:mm a")}
+          </p>
+        </div>
+        <p className={styles.activityWebsite}>
+          <a
+            className={styles.activityUrl}
+            href={`${activity.activityWebsite}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            {activity.name}'s website
+          </a>
+        </p>
+      </div>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.th}>Notes:</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className={styles.td}>{activity.notes}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
